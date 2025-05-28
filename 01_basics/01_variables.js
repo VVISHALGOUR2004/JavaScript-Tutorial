c1d1fb00271dc6b9081d1990af5f900f3c42043e
Vishal Gour <vishalgour624@gmail.com>
2025-05-28 04:33:53 +0000

    new Commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 1444    
+let email = "vvis@gmail.com"  
+var accountPassword = "12345";
+let accountState;
+email = "sandeepgmail.com "     
+accountPassword = "4545"   
+
+
+
+console.table([accountId,email,accountPassword])
