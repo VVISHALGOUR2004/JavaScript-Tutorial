5ba0a5f355ba68f577b2bef8ab3bc8f98ae871eb
Vishal Gour <vishalgour624@gmail.com>
2025-05-29 07:19:37 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -2,4 +2,32 @@
 
 // alert("Hello alert. I am here") alert will not work because we are using Node js not browser
 
+console.log(3 
+    +
+     3); // code readability should be high
+
+console.log("Vishal");
+
+
+
+// datatypes 
+let name = "Vishal";
+let age = 3;
+let isLoggedIn = true
+
+//                   primitive data types in js 
+// String: Represents textual data, enclosed in single or double quotes.
+// Number: Represents numeric values, including integers and floating-point numbers.
+// Boolean: Represents logical values, either true or false.
+// Null: Represents the intentional absence of any object value.
+// Undefined: Represents a variable that has been declared but not assigned a value. 
+// Symbol: Represents unique and immutable identifiers, often used as object keys. 
+// BigInt: Represents integers of arbitrary precision, larger than what the Number type can handle
+
+
+// Object 
+
+console.log(typeof  null); //Object
+console.log(typeof undefined); // undefined
+
 
