d173fbe1904b2462a8f5c92af360ff1ae6c0c01d
Vishal Gour <vishalgour624@gmail.com>
2025-05-29 09:42:51 +0000

    DataType

---
+++ 01_basics/03_conversationOperation.js
@@ -0,0 +1,33 @@
+// 1. Number
+
+// let score = "33asa"
+
+// let changeNum = Number(score)
+
+// console.log(typeof changeNum);
+// console.log(changeNum);
+
+// "33"=> 33
+// "33abcc" => NaN
+
+// 2. Boolean
+
+// let isLoggedIn = null;
+// let isBoolean = Boolean(isLoggedIn);
+
+// console.log(typeof isBoolean);
+// console.log(isBoolean);
+
+// 1 => true
+// "" => false
+// "Vishal" => true;
+// undefined => false;
+// null => false
+
+// Strings
+
+let someNum = 44;
+let stringNum = String(someNum);
+console.log(stringNum);
+console.log(typeof stringNum);
+console.log(typeof someNum);
