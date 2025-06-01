a8bb2707279edcaa4bdc00c78f45ee3c0f7484f9
Vishal Gour <vishalgour624@gmail.com>
2025-06-01 07:27:28 +0000

    stack and heap

--- datatypes_summary.js
+++ datatypes_summary.js
@@ -35,3 +35,44 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3 
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++
+
+
+// stack(primitive) , heap(Non-primitive)  
+
+
+//  stack 
+
+let name = "Vishal";
+let anotherName = name
+
+anotherName = "Sandeep Joshi"
+
+console.log(name);
+console.log(anotherName);
+
+// This is called stack because stack create copy of the variable if we create name variable and its value is "vishal" and at the next we create another variable which is equal to first variable but if we give second variable value to "Sandeep Joshi" so the another name value is be a Sandeep joshi.This is called Stack.
+
+
+// Non Primitive (Heap )
+
+let obj = {
+
+    namee : "Vishu",
+    email : "vis@abc.gmail.com"
+}
+
+
+let obj2 = obj
+
+obj2.namee = "ALLU"
+
+console.log(obj);
+console.log(obj2);
+
+
+
+
