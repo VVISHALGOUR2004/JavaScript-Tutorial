a48b62fe2c748e27e1d9c7d69cd63cd02ec2d37e
Vishal Gour <vishalgour624@gmail.com>
2025-06-03 06:48:07 +0000

    strings in js

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,34 @@
+//  const name = "vishal";
+//  const repoCount = 50
+
+// //  console.log(name +  repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+//  const nameGame = new String("Vishal Gour");
+//  console.log(nameGame.__proto__);
+
+//  console.log(nameGame.length);
+//   console.log(nameGame.toUpperCase());
+//   console.log(nameGame.toLowerCase());
+//   console.log(nameGame.charAt(4));
+
+//   const newStr = nameGame.substring(2,4)
+//   console.log(newStr);
+
+//   const anotherString = nameGame.slice(4,6)
+//   console.log(anotherString);
+
+//   const trimstr = "   vishal        "
+//   console.log(trimstr);
+//   console.log(trimstr.trim());
+
+//   const url = "https://chatgpt.com"
+
+//   console.log(url.replace('chat','vis'));
+
+//   console.log(url.includes("chat"));
+
+let harm = "Vishal Gour"
+let newharm = harm.split("")
+console.log(newharm);
