8636a5cf480917fe9222fae3b809f63bb576a8b4
Vishal Gour <vishalgour624@gmail.com>
2025-06-05 06:23:49 +0000

    nums_and_math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,37 @@
+// ************************** Numbers *****************************
+
+// const score = 500;
+
+
+// const balance = new Number(100.66)
+// console.log(balance);
+// console.log(score);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+
+// const otherNumber = 134.8983;
+
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString("en-IN"));
+ 
+
+
+// **************************** Maths *****************************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.2));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.5));
+// console.log(Math.min(4,5,7,8,6,3));
+// console.log(Math.max(4,4,7,8,9,5));
+
+
+
+let num = Math.floor(Math.random() * 10) + 1;
+console.log(num); // 1 to 10
