23d598a2ae3641a90ca3585cb3beb073a8e580e0
Vishal Gour <vishalgour624@gmail.com>
2025-06-05 06:55:48 +0000

    Date in  Javascript

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,33 @@
+
+// *** Dates 
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+
+// let mycreatedDate = new Date(2023 , 0 , 23)
+// let mycreatedDate = new Date(2023 , 0 , 23 ,5,3)
+let mycreatedDate = new Date("01-14-2024")
+
+// // console.log(mycreatedDate.toDateString());
+// console.log(mycreatedDate.toLocaleString());
+
+
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+
+
+
+// console.log(Math.floor(Date.now()/1000));
+
+////  in js month and days start with 0 index 
+
+// let newDate = new Date()
+// console.log(newDate.getDate());
+// console.log(newDate.getDay());
+// console.log(newDate.getHours());
+
