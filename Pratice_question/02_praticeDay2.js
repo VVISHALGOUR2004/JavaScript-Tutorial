6ec77fb9e1ccbae8604a5d3a7fc61a6e2906ecd3
Vishal Gour <vishalgour624@gmail.com>
2025-06-06 09:54:11 +0000

    pratice day 2

---
+++ Pratice_question/02_praticeDay2.js
@@ -0,0 +1,82 @@
+                                        //   9 ..... How to determine the number of elements in an array.
+
+// let arr = [56, 67, 88, 9, 34, 89, 91, 45];
+
+// console.log(arr.length);
+
+                                 // 10 ...... Reversing an array: How to reverse the order of elements in an array.
+
+// const arr1 = [6, 7, 89, 45, 78, 34, 56, 78, 98];
+
+// arr1.reverse();
+// console.log(arr1);
+
+                             // 11*********  Sorting an array: How to sort the elements in an array in ascending or descending order.
+
+// In sorting method if the arrays value is in the strings  elements as strings in alphabetical and ascending order.
+
+// let arr = ["Raju","Sonu","Papa", "Mummy"];
+// arr.sort()
+// console.log(arr);
+
+                           //12 *****  Filtering an array: How to create a new array with elements that meet a specific condition.
+
+// const num = [23,56,77,88,99,22];   // in n numbers
+
+// let filter = num.filter(num=> num>50)
+
+// console.log(filter);
+
+// in strings
+
+// In this solution our logic is  if the string have more than 5 charcter  that string add in to new array
+
+// const str = ["Vishal", "Nehass", "Yash", "Naveen"];
+
+// let newStr = str.filter((s) => s.length > 4);
+// console.log(newStr);
+
+                               // 13.... Merging arrays: How to combine two or more arrays into a single array.
+
+// In this method we use concat method
+
+// const arr = [34,56,77,88,99];
+// const arr11 = [44,66,77,88];
+// const arr122 = [45,67,8,55,44];
+
+// const final = arr.concat(arr11,arr122)
+// console.log(final);
+
+                            // 14.... Slicing an array: How to extract a portion of an array into a new array.
+
+// const fruits = ["mango","Apple","banana","papaya","Grapes"];
+
+// console.log(fruits.slice(2,4));
+
+                           //15...... Splicing an array: How to add or remove elements from an array at a specific position.
+// method 1
+
+// let vegi = ["potato","onion","LadyFinger","Tomato","Chillies "]
+// vegi.splice(1,3)
+// console.log(vegi);
+
+//  method 2
+//  in this method we add the value
+//
+// const fruits = ["apple", "banana", "mango"];
+
+// fruits.splice(1, 3, "kiwi", "grapes");
+
+// console.log(fruits);
+
+//method 3
+
+// we replace the value also
+// const fruits = ["apple", "banana", "mango"];
+
+// fruits.splice(1, 1, "pineapple");
+
+// console.log(fruits);
+
+
+
