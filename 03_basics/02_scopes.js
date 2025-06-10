686bb63fe452668bd0e51d53c0976c8dd3cf94e1
Vishal Gour <vishalgour624@gmail.com>
2025-06-10 06:10:32 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,23 +1,59 @@
 //var c = 300
-let a = 300;
+let a = 300
 if (true) {
-  let a = 10;
-  const b = 20;
-  // console.log("INNER: ", a);
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
 }
 
-// // console.log(a);
-// // console.log(b);
-// // console.log(c);
 
-function one() {
-  let firstvariable = "Hello My name is first variable";
-  function two() {
-    console.log(firstvariable);
-  }
-  return two;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
 }
 
-const final = one();
+// one()
 
-final()
+if (true) {
+    const username = "vishal"
+    if (username === "vishal") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(5)
